b388587beda8ba5a25a11e20731d032309e48938
shieldst1978 <36296160+shieldst1978@users.noreply.github.com>
2025-12-29 15:12:16 +0000

    added some movement behaviour

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
@@ -12,22 +13,33 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
         # Set background colour
-        self.bg_colour = (0, 0, 255)
-
+        
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
+            self._check_events()
+            # Redraw the screen during each pass through the loop
+            self._update_screen()                    
+            self.clock.tick(60)
+
+    def _check_events(self):
+        # respond to keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_colour)
+                elif event.type == pygame.KEYDOWN:
+                    if event.type == pygame.K_RIGHT:
+                        # Move the ship right
+                        self.ship.rect.x += 1
+                        
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_colour)
+        self.ship.blitme()
+        pygame.display.flip()
+         
 
 if __name__ == '__main__':
     # Make a game instance and run the game.
--- ship.py
+++ ship.py
@@ -8,7 +8,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
-        self.image =pygame.image.load('images/Ship.bmp')
+        self.image =pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom centre of the screen.
