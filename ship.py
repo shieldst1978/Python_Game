d85524ee99569dff7ef239591dec73fe744335d3
shieldst1978 <36296160+shieldst1978@users.noreply.github.com>
2025-12-29 15:23:47 +0000

    enged the movement attributes:
    :wq

--- ship.py
+++ ship.py
@@ -8,11 +8,18 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
-        self.image =pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom centre of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.moving_right = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """Draw the ship at its current location"""
